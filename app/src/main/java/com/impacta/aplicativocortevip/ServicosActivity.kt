ef6f02db1ffcd38c77a0e41307e78b7afd445f8b
Enzo <enzopcavinato@gmail.com>
2025-06-03 16:58:29 -0300

    navegação entre telas, unidades, serviços e mudanças no RA

--- app/src/main/java/com/impacta/aplicativocortevip/ServicosActivity.kt
+++ app/src/main/java/com/impacta/aplicativocortevip/ServicosActivity.kt
@@ -1,11 +1,20 @@
 package com.impacta.aplicativocortevip
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ServicosActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_servicos)
+
+        val btnAvancar = findViewById<Button>(R.id.btn_avancar_servico)
+        btnAvancar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
     }
 }
